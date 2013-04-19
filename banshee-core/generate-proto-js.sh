c724a78e83ff0e145f7f211830d33d506a2001d7
Guillaume Le Biller <guillaume.lebiller@gmail.com>
2013-04-19 12:00:12 +0200

    Added maven pom to banshee-core-module

--- banshee-core/generate-proto-js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-proto2js Proto/Common/Property.proto -class > Js/Property.js
-proto2js Proto/Debug/ObjectProperty.proto -class > Js/ObjectProperty.js
-proto2js Proto/Debug/Object.proto -class > Js/Object.js
-proto2js Proto/Debug/Debug.proto -class > Js/Debug.js
--- banshee-core/generate-proto.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-protoc Proto/Debug/*.proto --cpp_out=Cpp --java_out=Java
-protoc Proto/Common/*.proto --cpp_out=Cpp --java_out=Java
